4bbab3856c3bbbf61a89fa8c4a39d3149298f5b5
mukhammadmirzo <mukhammadmirzo078@gmail.com>
2026-08-21 18:01:15 +0500

    added created_at field

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,7 @@ class Article(models.Model):
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True,
         related_name="articles",)
+    created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.title
     def save(self, *args, **kwargs):
