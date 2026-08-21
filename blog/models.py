28614239eada0d2082ffdc45c87d8515240fe485
Toxa <fedatohirov@gmail.com>
2026-08-21 17:39:23 +0500

    Article model added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.text import slugify
+from django.conf import settings
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=220, unique=True, blank=True)
+    content = models.TextField()
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True,
+        related_name="articles",)
+    def __str__(self):
+        return self.title
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
