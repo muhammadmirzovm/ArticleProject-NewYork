20ef07f22d106b3981bf59400607c50811875f26
mukhammadmirzo <mukhammadmirzo078@gmail.com>
2026-08-21 19:12:23 +0500

    added templates and views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Article
 
-# Create your views here.
+def article_list(request):
+   articles = Article.objects.all().order_by("-created_at")
+   return render(request, "blog/article_list.html", {"articles": articles})
+
+def article_detail(request, slug):
+   article = get_object_or_404(Article, slug=slug)
+   return render(request, "blog/article_detail.html", {"article": article})
