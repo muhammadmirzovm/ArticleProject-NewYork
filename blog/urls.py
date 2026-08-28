c8a31c23a8fbace01e73da430376fac55e536d7f
mukhammadmirzo <mukhammadmirzo078@gmail.com>
2026-08-28 18:12:21 +0500

    added article create

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.article_list, name='article_list'),
-    path('articles/<slug:slug>', views.article_detail, name='article_detail')
+    path('articles/<slug:slug>', views.article_detail, name='article_detail'),
+    path('article/create/', views.article_create, name='article_create'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Article
+from django.contrib.auth.decorators import login_required
+from .forms import ArticleForm
 
 def article_list(request):
    articles = Article.objects.all().order_by("-created_at")
@@ -11,3 +13,13 @@ def article_detail(request, slug):
 
 
 
+
+@login_required
+def article_create(request):
+   form = ArticleForm(request.POST or None, request.FILES or None)
+   if request.method == "POST" and form.is_valid():
+       article = form.save(commit=False)
+       article.author = request.user
+       article.save()
+       return redirect("article_detail", slug=article.slug)
+   return render(request, "blog/article_form.html", {"form": form})
